9420572b3ade9017b9124737741423347d681ed5
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-14 09:26:17 +0530

    Step 10: check_anti_diagonal_win

--- model.py
+++ model.py
@@ -86,8 +86,12 @@ def check_main_diagonal_win(board, player):
     # TODO: check whether the main diagonal of `board` is fully occupied by `player`...
     return np.all(board.diagonal() == player)
 
-# Step 10 - check_anti_diagonal_win (not yet solved)
-# TODO: implement
+# Step 10 - check_anti_diagonal_win
+import numpy as np
+
+def check_anti_diagonal_win(board, player):
+    # TODO: return True if `player` occupies all three anti-diagonal cells of the 3x3 board.
+    return np.all(np.fliplr(board).diagonal() == player)
 
 # Step 11 - is_winner (not yet solved)
 # TODO: implement
